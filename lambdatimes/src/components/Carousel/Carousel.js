9de4b68296f395ea2e6097c5ea1acad0e9810907
Karim B <mattiakarim@live.it>
2019-06-07 10:33:32 +0200

    Complete carousel for stretch goal, now working on HOC and login page

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -9,6 +9,7 @@ export default class Carousel extends Component {
       counter: 0
     }
   }
+  
   componentDidMount(){
     this.setState({
       carousel: carouselData,
@@ -16,21 +17,40 @@ export default class Carousel extends Component {
   }
 
   leftClick = () => {
-
+    if(this.state.counter !== 0) {
+      this.setState(prevState => ({
+        counter: prevState.counter - 1
+      }))
+    } else {
+      this.setState(prevState => ({
+        counter: prevState.carousel.length - 1
+      }))
+    }
   }
 
   rightClick = () => {
-
+    if(this.state.counter !== this.state.carousel.length - 1) {
+      this.setState(prevState => ({
+        counter: prevState.counter + 1
+      }))
+    } else {
+      this.setState({
+        counter: 0
+      })
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.carousel[this.state.counter]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {
+          this.selectedImage()
+        }
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
